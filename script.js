73c3d55ec5cea0e9c21ea9aa189624b0248515b3
Spartan-Ashwin <123223152+Spartan-Ashwin@users.noreply.github.com>
2023-11-17 15:05:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let btn=document.querySelector("#enterBtn");
+
+btn.addEventListener("click",()=>{
+	let p=document.querySelector("#status");
+	p.innerText="Entered Metaverse"
+})
